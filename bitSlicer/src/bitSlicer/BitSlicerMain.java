601eefe85caefa5eb62900eb5bff02a718dacbdb
Thibault <Thibault@192.168.1.91>
2016-11-09 16:54:31 +0100

    Cleaning

--- bitSlicer/src/bitSlicer/BitSlicerMain.java
+++ bitSlicer/src/bitSlicer/BitSlicerMain.java
@@ -1,35 +1,24 @@
 package bitSlicer;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Vector;
 
-import javax.swing.SwingUtilities;
-
-import bitSlicer.Slicer.Config.CraftConfigLoader;
-import bitSlicer.gui.ConfigWindow;
-import bitSlicer.Layer;
-import bitSlicer.Model;
-import bitSlicer.Slicer.Config.CraftConfig;
 import bitSlicer.Slicer.Slice;
 import bitSlicer.Slicer.SliceTool;
-import bitSlicer.gui.PreviewFrame;
+import bitSlicer.Slicer.Config.CraftConfig;
+import bitSlicer.Slicer.Config.CraftConfigLoader;
+import bitSlicer.gui.ConfigWindow;
 import bitSlicer.util.Logger;
-import bitSlicer.util.Vector2;
 
 public class BitSlicerMain {
 	public static void main(String[] args)
 	{
 		CraftConfigLoader.loadConfig(null);
-		
 		new ConfigWindow();
-		
-		CraftConfigLoader.saveConfig(null);
 	}
 	
 	public static void sliceModel(String filename)
 	{
-		long startTime = System.currentTimeMillis();
 		CraftConfig.lastSlicedFile = filename;
 		CraftConfigLoader.saveConfig(null);
 
