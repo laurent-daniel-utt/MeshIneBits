f6172f4a42e5e1747b4cc826c79e75ed8510aefb
Thibault <Thibault@192.168.1.91>
2016-11-21 22:57:32 +0100

    Bit2D is now an Area and not a Shape2D

--- bitSlicer/src/bitSlicer/Bit2D.java
+++ bitSlicer/src/bitSlicer/Bit2D.java
@@ -1,5 +1,7 @@
 package bitSlicer;
 
+import java.awt.geom.Area;
+import java.awt.geom.Path2D;
 import java.util.Vector;
 
 import bitSlicer.Slicer.Slice;
@@ -7,13 +9,14 @@ import bitSlicer.Slicer.Config.CraftConfig;
 import bitSlicer.util.Shape2D;
 import bitSlicer.util.Vector2;
 import bitSlicer.util.AABBTree;
+import bitSlicer.util.AreaTool;
 import bitSlicer.util.Polygon;
 import bitSlicer.util.Segment2D;;
 
 /**
  * Bit2D represents a bit in 2d space.
  */
-public class Bit2D extends Shape2D {
+public class Bit2D extends Area {
 	public Vector2 origin;
 	private Vector2 orientation;
 	private double length;
@@ -34,76 +37,23 @@ public class Bit2D extends Shape2D {
 		Vector2 cornerUpLeft = new Vector2(- length/2.0,  - width/2.0);
 		Vector2 cornerUpRight = new Vector2(+ length/2.0,  - width/2.0);
 		Vector2 cornerDownLeft = new Vector2(- length/2.0,  + width/2.0);
-		Vector2 cornerDownRight = new Vector2(+ length/2.0,  + width/2.0);
+		Vector2 cornerDownRight = new Vector2(+ length/2.0,  + width/2.0);       
 		
-		Segment2D top = new Segment2D(1, cornerUpLeft, cornerUpRight);
-		Segment2D right = new Segment2D(1, cornerUpRight, cornerDownRight);
-		Segment2D bottom = new Segment2D(1, cornerDownRight, cornerDownLeft);
-		Segment2D left = new Segment2D(1, cornerDownLeft, cornerUpLeft);
-		
-		//We could just do a optimize() but we save a lot of time by doing it like that
-		top.setNext(right);
-		right.setNext(bottom);
-		bottom.setNext(left);
-		left.setNext(top);
-		
-		addModelSegment(top);
-		addModelSegment(right);
-		addModelSegment(bottom);
-		addModelSegment(left);
-		
-		polygons.add(new Polygon(top));
-	}
-	
-	public void setBitInPatternCooSystem(){
-		this.setInLowerCooSystem(orientation, origin);
-	}
-	
-	public void setInThatCooSystem(Vector2 rotation, Vector2 offSet){
-		setInLowerCooSystem(rotation, offSet);
-		origin = origin.getInLowerCooSystem(rotation, offSet);
+		Path2D path = new Path2D.Double();
+		path.moveTo(cornerUpLeft.x, cornerUpLeft.y);			
+		path.lineTo(cornerUpRight.x, cornerUpRight.y);
+		path.lineTo(cornerDownRight.x, cornerDownRight.y);
+		path.lineTo(cornerDownLeft.x, cornerDownLeft.y);
+		path.closePath();
+
+		this.add(new Area(path));
 	}
 	
 	/**
 	 * Check if the bit is on the outline of the slice
 	 */
 	public boolean isOnPath(Slice slice) {
-		for(Polygon p : this){
-			for (Segment2D b : p) {
-				for (Segment2D s : slice.getSegmentList()) {
-					if (b.getCollisionPoint(s) != null) return true;
-				}
-			}
-		}
-		return false;	
-	}
-	
-	public void setNewBoundaries(Shape2D slice){
-		Vector<Segment2D> newBoundariesSegment = new Vector<Segment2D>();
-		
-		for (Segment2D s : this.trim(slice).getSegmentList()){
-			newBoundariesSegment.add(new Segment2D(1, s.start, s.end)); // We can't add directly "s" because we need to erase any prev/next segment previously stored
-		}
-		
-		polygons.clear();
-		segmentList.clear();
-		segmentTree = new AABBTree<Segment2D>();
-		
-		
-		
-		//newSegments = optimizeDirections(removeUnwantedSegments(newBoundariesSegment));
-		//newSegments = optimizeDirections(newBoundariesSegment);
-		for(Segment2D s : newBoundariesSegment){
-			this.addModelSegment(s);
-		}
-//		if(newSegments.size() > 2){
-//			optimize2();
-//		}
-//		else{
-//			System.out.println("Not enough segments");
-//		}
-		
-		this.optimize();
+		return (AreaTool.getAreaFrom(slice).intersects(this.getBounds()));
 	}
 	
 	/**
@@ -116,11 +66,9 @@ public class Bit2D extends Shape2D {
 			return slice.contains(this.origin);
 	}
 	
-	/**
-	 * WIP - Return the a cut bit without the excess of material outside the slice
-	 * TODO update this
-	 */
-	public Shape2D getTruncatedBitShape(Slice slice) {
-		return this.trim(slice);
+	public Area getArea(Slice slice) {
+		Area truncatedArea = (Area) this.clone();
+		truncatedArea.intersect(AreaTool.getAreaFrom(slice));
+		return truncatedArea;
 	}
 }
