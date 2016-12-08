5f87ed525fbc4de96b3c9a12f6d902bf203d90ce
Ull <thibault.cassard@gmail.com>
2016-12-08 15:37:05 +0100

    Update

--- bitSlicer/src/bitSlicer/gui/MainWindow.java
+++ bitSlicer/src/bitSlicer/gui/MainWindow.java
@@ -50,29 +50,38 @@ public class MainWindow extends JFrame {
 
 	private class Ribbon extends JTabbedPane {
 		public Ribbon() {
-//			setMaximumSize(new Dimension(1000000, 100));
 			setMinimumSize(new Dimension(100, 200));
-			addTab("File", new FileTab());
+			addTab("Slicer", new SlicerTab());
 			addTab("Edit", new JSpinner(new SpinnerNumberModel(5, 0, 10, 1)));
-			
-			
 		}
 		
-		private class FileTab extends JPanel {
-			public FileTab() {
+		private class SlicerTab extends JPanel {
+			public SlicerTab() {
 				this.setLayout(new FlowLayout(FlowLayout.LEFT));
 				
 				JPanel fichierContainer = new JPanel();
 				fichierContainer.setLayout(new GridLayout(3, 0));
-				
 				fichierContainer.setBorder(BorderFactory.createTitledBorder("Fichier"));
-				fichierContainer.add(new Button("Nouveau"));
-				fichierContainer.add(new Button("Enregistrer"));
-				fichierContainer.add(new Button("Quitter"));
-				fichierContainer.add(new Button("Nouveau"));
-				fichierContainer.add(new Button("Enregistrer"));
-				
+				fichierContainer.add(new Button("New"));
+				fichierContainer.add(new Button("Save"));
+				fichierContainer.add(new Button("Close"));				
 				add(fichierContainer);
+				
+				JPanel slicerContainer = new JPanel();
+				slicerContainer.setLayout(new GridLayout(3, 0));
+				slicerContainer.setBorder(BorderFactory.createTitledBorder("Slicer options"));
+				
+				JPanel spinPan = new JPanel(new BorderLayout());
+				spinPan.add(new JLabel("Slice height (mm) :  "), BorderLayout.WEST);
+				spinPan.add(new JSpinner(new SpinnerNumberModel(5, 0, 10, 1)), BorderLayout.EAST);
+				slicerContainer.add(spinPan);
+				
+				JPanel spinPan2 = new JPanel(new BorderLayout());
+				spinPan2.add(new JLabel("First slice height (%) :  "), BorderLayout.WEST);
+				spinPan2.add(new JSpinner(new SpinnerNumberModel(5, 0, 10, 1)), BorderLayout.EAST);
+				slicerContainer.add(spinPan2);
+				
+				add(slicerContainer);
 			}
 		}
 		
