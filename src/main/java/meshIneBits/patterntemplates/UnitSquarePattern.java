80a91868407d3e8f1a9debdb736d313925fdacb8
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-03-20 11:51:18 +0100

    Add UnitMatrix.

--- src/main/java/meshIneBits/patterntemplates/UnitSquarePattern.java
+++ src/main/java/meshIneBits/patterntemplates/UnitSquarePattern.java
@@ -3,7 +3,9 @@
  */
 package meshIneBits.patterntemplates;
 
+import java.awt.geom.Area;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
@@ -12,8 +14,12 @@ import meshIneBits.Bit2D;
 import meshIneBits.GeneratedPart;
 import meshIneBits.Layer;
 import meshIneBits.Pattern;
+import meshIneBits.config.CraftConfig;
 import meshIneBits.config.PatternParameterConfig;
+import meshIneBits.slicer.Slice;
+import meshIneBits.util.AreaTool;
 import meshIneBits.util.Logger;
+import meshIneBits.util.Shape2D;
 import meshIneBits.util.Vector2;
 
 /**
@@ -46,8 +52,15 @@ import meshIneBits.util.Vector2;
  *
  */
 public class UnitSquarePattern extends PatternTemplate {
-	
-	private Set<UnitSquare> unitSquares = new HashSet<UnitSquare>();
+
+	/**
+	 * Equal to horizontal margin plus lift point diameter
+	 */
+	private double unitLength;
+	/**
+	 * Equal to vertical margin plus lift point diameter
+	 */
+	private double unitHeight;
 
 	/*
 	 * (non-Javadoc)
@@ -56,13 +69,8 @@ public class UnitSquarePattern extends PatternTemplate {
 	 */
 	@Override
 	public void initiateConfig() {
-		// TODO Auto-generated method stub
-		// Unit square's length
-		// = lift point diameter + horizontal margin
 		config.add(new PatternParameterConfig("horizontalMargin", "Horizontal margin",
 				"A little space allowing Lift Point move horizontally", 1.0, 100.0, 2.0, 1.0));
-		// Unit square's width
-		// = lift point diameter + vertical margin
 		config.add(new PatternParameterConfig("verticalMargin", "Vertical margin",
 				"A little space allowing Lift Point move vertically", 1.0, 100.0, 2.0, 1.0));
 	}
@@ -75,6 +83,7 @@ public class UnitSquarePattern extends PatternTemplate {
 	 */
 	/**
 	 * This method does nothing.
+	 * 
 	 * @return <tt>false</tt>
 	 */
 	@Override
@@ -103,10 +112,29 @@ public class UnitSquarePattern extends PatternTemplate {
 	 */
 	@Override
 	public int optimize(Layer actualState) {
-		// TODO Implement
 		Logger.updateStatus("Paving layer " + actualState.getLayerNumber());
-		
+		// Calculate size of unit square
+		this.calcUnitSize();
+		// Get the boundary
+		Vector<Area> zones = AreaTool.getLevel0AreasFrom(actualState.getSelectedSlice());
+		// Sum of pavement
+		Vector<Bit2D> overallPavement = new Vector<Bit2D>();
+		for (Area zone : zones) {
+			// Generate the corresponding matrix
+			UnitMatrix matrix = new UnitMatrix(zone);
+			matrix.resolve();
+			overallPavement.addAll(matrix.exportBits());
+		}
+		actualState.setReferentialPattern(new Pattern(overallPavement, new Vector2(1, 0)));
 		return 0;
+	}
+
+	/**
+	 * Calculate unit's size
+	 */
+	private void calcUnitSize() {
+		this.unitLength = ((double) config.get("horizontalMargin").getCurrentValue()) + CraftConfig.suckerDiameter;
+		this.unitHeight = ((double) config.get("verticalMargin").getCurrentValue()) + CraftConfig.suckerDiameter;
 	}
 
 	/*
@@ -155,13 +183,111 @@ public class UnitSquarePattern extends PatternTemplate {
 	public String getHowToUse() {
 		return "Unit square's size should be a little bit larger than Lift Points, so that we have safe space between bits.";
 	}
-	
+
 	private class UnitSquare extends Rectangle2D.Double {
+		public UnitSquare(double x, double y) {
+			super(x, y, unitLength, unitHeight);
+		}
+
 		public UnitState state;
-	}
-	
-	enum UnitState {
-		ACCEPTED, BORDER, IGNORED
+
+		/**
+		 * Find the state of this unit
+		 * 
+		 * @param area
+		 *            where we will fill out by bits
+		 */
+		public void determineState(Area area) {
+			if (area.contains(this)) {
+				this.state = UnitState.ACCEPTED;
+			} else if (area.intersects(this)) {
+				this.state = UnitState.BORDER;
+			} else {
+				this.state = UnitState.IGNORED;
+			}
+		}
 	}
 
+	/**
+	 * Describe the relative position in respect to predefined area
+	 * 
+	 * @author Quoc Nhat Han TRAN
+	 *
+	 */
+	enum UnitState {
+		/**
+		 * Unit totally inside area, possibly touching boundary
+		 */
+		ACCEPTED,
+		/**
+		 * Unit partially inside area, not counted if only touching boundary
+		 */
+		BORDER,
+		/**
+		 * Unit totally outside area
+		 */
+		IGNORED
+	}
+
+	private class UnitMatrix {
+		private UnitSquare[][] matrix;
+		private Area area;
+
+		/**
+		 * Prepare buckets to hold units
+		 * 
+		 * @param area
+		 *            a level 0 area
+		 */
+		public UnitMatrix(Area area) {
+			this.area = area;
+			Rectangle2D.Double outerRect = (Double) this.area.getBounds2D();
+			int numOfUnitsInLength = (int) Math.ceil(outerRect.getWidth() / unitLength);
+			int numOfUnitsInHeight = (int) Math.ceil(outerRect.getHeight() / unitHeight);
+			this.matrix = new UnitSquare[numOfUnitsInHeight][numOfUnitsInLength];
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[i].length; j++) {
+					this.matrix[i][j] = new UnitSquare(outerRect.getX() + j * unitLength,
+							outerRect.getY() + i * unitHeight);
+					this.matrix[i][j].determineState(area);
+				}
+			}
+		}
+
+		/**
+		 * Construct bits from units
+		 * 
+		 * @return bits regrouped from units
+		 */
+		public Vector<Bit2D> exportBits() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		/**
+		 * Concatenate {@link UnitState#BORDER border} units with
+		 * {@link UnitState#ACCEPTED accepted} one
+		 */
+		public void resolve() {
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[i].length; j++) {
+					UnitSquare unit = this.matrix[i][j];
+					switch (unit.state) {
+					case IGNORED:
+						// Do nothing
+						break;
+					case ACCEPTED:
+						// Do nothing
+						break;
+					case BORDER:
+						// Check neighbors
+						// TODO Auto-generated method stub
+						break;
+					default:
+						break;
+					}
+				}
+			}
+		}
+	}
 }
