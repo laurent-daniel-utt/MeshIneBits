beda2d1aed14e0163d7922ab8b9978924cb3bcaa
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-04-16 22:29:37 +0200

    Add simple tests

---
+++ src/test/java/meshIneBitsTest/ExampleTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package meshIneBitsTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Quoc Nhat Han TRAN
+ *
+ */
+class ExampleTest {
+
+	@Test
+	void test() {
+		System.out.println("This test method should be run");
+	}
+
+}
