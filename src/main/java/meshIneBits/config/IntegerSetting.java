d99d028589ccba185bc1b583d498afc683c6d1e2
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-11-29 21:57:26 +0100

    Make annotation documented

--- src/main/java/meshIneBits/config/IntegerSetting.java
+++ src/main/java/meshIneBits/config/IntegerSetting.java
@@ -22,13 +22,11 @@
 
 package meshIneBits.config;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@Documented
 public @interface IntegerSetting {
     double step() default 1;
 
