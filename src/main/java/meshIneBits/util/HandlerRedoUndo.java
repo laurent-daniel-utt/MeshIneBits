a32d91c7eeaa9496bea0ed785633e37ac12ce8f9
doquangbao <51085401+doquangbao@users.noreply.github.com>
2020-09-29 23:41:29 +0200

    undo/redo of cutted bits (fixed) - akk for file replacing if file exists (fixed)
    can undo/redo of cutted bits now
    and ask for file replacing if file exist is added

--- src/main/java/meshIneBits/util/HandlerRedoUndo.java
+++ src/main/java/meshIneBits/util/HandlerRedoUndo.java
@@ -2,41 +2,83 @@ package meshIneBits.util;
 
 
 import meshIneBits.Bit3D;
+import meshIneBits.config.CraftConfig;
 import meshIneBits.gui.view2d.MeshController;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  *
  * */
 public class HandlerRedoUndo{
-    private final Stack<ActionMoveBit> previousActionBits;
-    private final Stack<ActionMoveBit> afterActionBits;
+    private final Stack<ActionOfUser> previousActionOfUserBits;
+    private final Stack<ActionOfUser> afterActionOfUserBits;
 
     public HandlerRedoUndo() {
-        this.previousActionBits = new Stack<>();
-        this.afterActionBits = new Stack<>();
+        this.previousActionOfUserBits = new Stack<ActionOfUser>();
+        this.afterActionOfUserBits = new Stack<>();
     }
+    private interface ActionOfUser {
 
+        public void runUndo(MeshController meshController);
+        public void runRedo(MeshController meshController);
+    }
     public interface UndoFunction {
         void undo();
         void redo();
     }
 
-//    public void pushAction(ActionMoveBit a){
-//        this.actionBits.push(a);
-//    }
-
-    public Stack<ActionMoveBit> getPreviousActionBits() {
-        return previousActionBits;
+    public Stack<ActionOfUser> getPreviousActionOfUserBits() {
+        return previousActionOfUserBits;
     }
 
-    public Stack<ActionMoveBit> getAfterActionBits() {
-        return afterActionBits;
+    public Stack<ActionOfUser> getAfterActionOfUserBits() {
+        return afterActionOfUserBits;
+    }
+    /**
+    * Save actions scaling bits
+    * */
+    public static class ActionOfUserScaleBit implements ActionOfUser {
+        private double setPercentageLength;
+        private double setPercentageWidth;
+        private Map<Vector2,Double[]> keyMapLengthWidth;
+
+
+        public ActionOfUserScaleBit(Map<Vector2,Double[]> keyMapLengthWidth,double setPercentageLength, double setPercentageWidth) {
+            this.setPercentageLength = setPercentageLength;
+            this.setPercentageWidth = setPercentageWidth;
+            this.keyMapLengthWidth =keyMapLengthWidth;
+        }
+
+        @Override
+        public void runUndo(MeshController meshController) {
+            meshController.setSelectedBitKeys(this.keyMapLengthWidth.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toSet()));
+            if (meshController.getSelectedBitKeys().isEmpty()) {
+                Logger.warning("There is no bit selected");
+            } else {
+                meshController.setSelectedBitKeys(meshController.getSelectedBits().stream()
+                        .map(bit -> meshController.getCurrentLayer().scaleBit(bit,(this.keyMapLengthWidth.get(bit.getOrigin())[0]/ CraftConfig.bitLength)*100, (this.keyMapLengthWidth.get(bit.getOrigin())[1] / CraftConfig.bitWidth)*100))
+                        .collect(Collectors.toSet()));
+            }
+        }
+
+        @Override
+        public void runRedo(MeshController meshController) {
+            meshController.setSelectedBitKeys(this.keyMapLengthWidth.entrySet().stream().map(Map.Entry::getKey).collect(Collectors.toSet()));
+            if (meshController.getSelectedBitKeys().isEmpty()) {
+                Logger.warning("There is no bit selected");
+            } else {
+                meshController.setSelectedBitKeys(meshController.getSelectedBits().stream()
+                        .map(bit -> meshController.getCurrentLayer().scaleBit(bit,setPercentageLength, setPercentageWidth))
+                        .collect(Collectors.toSet()));
+            }
+        }
     }
     /**
      * Class that create the action did by user
      * */
-    public static class ActionMoveBit{
+    public static class ActionOfUserMoveBit implements ActionOfUser {
 
         /**
          * Clone of bits before transformation
@@ -51,7 +93,7 @@ public class HandlerRedoUndo{
         private Set<Vector2> previousSelectedBits =new HashSet<Vector2>();
 
 
-        public ActionMoveBit(Set<Bit3D> previousState,Set<Vector2> previousSelectedBits, Set<Vector2> resultKeys,Set<Bit3D> currentSelectedBits,int layerNumber) {
+        public ActionOfUserMoveBit(Set<Bit3D> previousState, Set<Vector2> previousSelectedBits, Set<Vector2> resultKeys, Set<Bit3D> currentSelectedBits, int layerNumber) {
             if(previousState!=null){
                 this.previousState = new HashSet<Bit3D>(previousState);
             }
@@ -67,7 +109,7 @@ public class HandlerRedoUndo{
             }
             this.layerNum=layerNumber;
         }
-        public ActionMoveBit(Set<Vector2> resultKeys,Set<Bit3D> currentSelectedBits,int layerNum){
+        public ActionOfUserMoveBit(Set<Vector2> resultKeys, Set<Bit3D> currentSelectedBits, int layerNum){
             this(null,null,resultKeys,currentSelectedBits,layerNum);
         }
 
@@ -78,56 +120,64 @@ public class HandlerRedoUndo{
         public Collection<Bit3D> getPreviousState() {
             return previousState;
         }
+
+        @Override
+        public void runUndo(MeshController meshController) {
+            meshController.setLayer(this.layerNum);
+            // Remove the latest bits with registered result keys
+            if(this.currentSelectedBits!=null&&this.resultKeys!=null){
+                meshController.deleteBitsByBitsAndKeys(this.currentSelectedBits,this.resultKeys);
+            }
+            // Restore deleted bits
+            // By adding the previous state
+            if(this.previousState!=null&&this.previousState.size()!=0){
+                meshController.addBit3Ds(this.previousState);
+                meshController.setSelectedBitKeys(this.previousSelectedBits);
+            }
+        }
+
+        @Override
+        public void runRedo(MeshController meshController) {
+            meshController.setLayer(this.layerNum);
+            // Remove the latest bits with registered result keys
+            meshController.setSelectedBitKeys( this.previousSelectedBits);
+            if(this.previousState!=null&&this.previousState.size()!=0){
+                meshController.deleteBitsByBitsAndKeys(this.previousState,this.previousSelectedBits);
+
+            }
+            // Restore deleted bits
+            // By adding the previous state
+            if(this.currentSelectedBits!=null){
+                meshController.addBit3Ds(this.currentSelectedBits);
+                meshController.setSelectedBitKeys(this.resultKeys);
+            }
+        }
     }
     //clear afterActionBit when create new action
-    public void addActionBit(ActionMoveBit actionMoveBit){
-        if(this.afterActionBits.size()!=0){
-            this.afterActionBits.clear();
+    public void addActionBit(ActionOfUser actionMoveBit){
+        if(this.afterActionOfUserBits.size()!=0){
+            this.afterActionOfUserBits.clear();
         }
-        this.previousActionBits.push(actionMoveBit);
+        this.previousActionOfUserBits.push(actionMoveBit);
     }
     /**
      * call by MeshController, for redoing the previous actions
      * */
     public void undo(MeshController meshController){
-        System.out.println("Before: "+previousActionBits.size());
-        if(previousActionBits !=null && previousActionBits.size()!=0){
-            HandlerRedoUndo.ActionMoveBit lastActionMoveBit = previousActionBits.pop();
-            afterActionBits.add(lastActionMoveBit);
-            meshController.setLayer(lastActionMoveBit.layerNum);
-            // Remove the latest bits with registered result keys
-            if(lastActionMoveBit.currentSelectedBits!=null&&lastActionMoveBit.resultKeys!=null){
-                meshController.deleteBitsByBitsAndKeys(lastActionMoveBit.currentSelectedBits,lastActionMoveBit.resultKeys);
-            }
-            // Restore deleted bits
-            // By adding the previous state
-            if(lastActionMoveBit.previousState!=null&&lastActionMoveBit.previousState.size()!=0){
-                meshController.addBit3Ds(lastActionMoveBit.previousState);
-                meshController.setSelectedBitKeys(lastActionMoveBit.previousSelectedBits);
-            }
+        if(previousActionOfUserBits !=null && previousActionOfUserBits.size()!=0) {
+            ActionOfUser lastAction = previousActionOfUserBits.pop();
+            afterActionOfUserBits.add(lastAction);
+            lastAction.runUndo(meshController);
         }
     }
     /**
      * call by MeshController, for redoing the following actions
      * */
     public void redo(MeshController meshController){
-        System.out.println("After: "+afterActionBits.size());
-        if(afterActionBits !=null && afterActionBits.size()!=0){
-            HandlerRedoUndo.ActionMoveBit lastActionMoveBit = afterActionBits.pop();
-            previousActionBits.add(lastActionMoveBit);
-            meshController.setLayer(lastActionMoveBit.layerNum);
-            // Remove the latest bits with registered result keys
-            meshController.setSelectedBitKeys( lastActionMoveBit.previousSelectedBits);
-            if(lastActionMoveBit.previousState!=null&&lastActionMoveBit.previousState.size()!=0){
-                meshController.deleteBitsByBitsAndKeys(lastActionMoveBit.previousState,lastActionMoveBit.previousSelectedBits);
-
-            }
-            // Restore deleted bits
-            // By adding the previous state
-            if(lastActionMoveBit.currentSelectedBits!=null){
-                meshController.addBit3Ds(lastActionMoveBit.currentSelectedBits);
-                meshController.setSelectedBitKeys(lastActionMoveBit.resultKeys);
-            }
+        if(afterActionOfUserBits !=null && afterActionOfUserBits.size()!=0) {
+            ActionOfUser lastAction = afterActionOfUserBits.pop();
+            previousActionOfUserBits.add(lastAction);
+            lastAction.runRedo(meshController);
         }
     }
 
