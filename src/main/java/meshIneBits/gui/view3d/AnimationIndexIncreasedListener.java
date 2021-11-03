12900582f2a341f2cedaba903c36f949968d5f22
doquangbao <51085401+doquangbao@users.noreply.github.com>
2021-11-03 18:40:06 +0100

    Organize multi threads for tasks execution Improve render performance of 3D

---
+++ src/main/java/meshIneBits/gui/view3d/AnimationIndexIncreasedListener.java
@@ -0,0 +1,7 @@
+package meshIneBits.gui.view3d;
+
+public interface AnimationIndexIncreasedListener {
+    void updateIndexRange(int min, int max);
+
+    void onIndexIncreasedListener(int index);
+}
