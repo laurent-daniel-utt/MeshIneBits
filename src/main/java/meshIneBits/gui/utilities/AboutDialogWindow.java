0f4cda26da7ec142738e0a091bbf3b0266640a74
HugoHugoHugoHugoHugo <hhugo53810@gmail.com>
2026-08-21 13:37:39 +0200

    Updating Copyrights with Intellij Idea's Copyright plugin : Added : files that describe : the copyright template to apply to files and the location of the affected files Modified : updated existing copyrights and added copyrights in files where it was missing
    Updated copyrights in the about section of the application :
    Modified AboutDialogWindow.java : added a JLabel per missing contributor and resized the window.

--- src/main/java/meshIneBits/gui/utilities/AboutDialogWindow.java
+++ src/main/java/meshIneBits/gui/utilities/AboutDialogWindow.java
@@ -1,32 +1,37 @@
-/*
- * MeshIneBits is a Java software to disintegrate a 3d mesh (model in .stl)
- * into a network of standard parts (called "Bits").
- *
- * Copyright (C) 2016-2022 DANIEL Laurent.
- * Copyright (C) 2016  CASSARD Thibault & GOUJU Nicolas.
- * Copyright (C) 2017-2018  TRAN Quoc Nhat Han.
- * Copyright (C) 2018 VALLON Benjamin.
- * Copyright (C) 2018 LORIMER Campbell.
- * Copyright (C) 2018 D'AUTUME Christian.
- * Copyright (C) 2019 DURINGER Nathan (Tests).
- * Copyright (C) 2020-2021 CLAIRIS Etienne & RUSSO André.
- * Copyright (C) 2020-2021 DO Quang Bao.
- * Copyright (C) 2021 VANNIYASINGAM Mithulan.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- */
+/*------------------------------------------------------------------------------
+ -  MeshIneBits is a Java software to disintegrate a 3d mesh (model in .stl)
+ -  into a network of standard parts (called "Bits").
+ -
+ -  Copyright (C) 2016-2026 DANIEL Laurent.
+ -  Copyright (C) 2015 Cédric Siourakhan
+ -  Copyright (C) 2016 Gabriel Magny
+ -  Copyright (C) 2016  CASSARD Thibault & GOUJU Nicolas.
+ -  Copyright (C) 2017-2018  TRAN Quoc Nhat Han.
+ -  Copyright (C) 2018 VALLON Benjamin.
+ -  Copyright (C) 2018 LORIMER Campbell.
+ -  Copyright (C) 2018 D'AUTUME Christian.
+ -  Copyright (C) 2019 DURINGER Nathan (Tests).
+ -  Copyright (C) 2020-2021 CLAIRIS Etienne & RUSSO André.
+ -  Copyright (C) 2020-2021 DO Quang Bao.
+ -  Copyright (C) 2021 VANNIYASINGAM Mithulan.
+ -  Copyright (C) 2021  Quang Long
+ -  Copyright (C) 2022 Mhamad Atlab
+ -  Copyright (C) 2022 Majed Hlaihel
+ -  Copyright (C) 2026 Aymeric Sirejol
+ -
+ -  This program is free software: you can redistribute it and/or modify
+ -  it under the terms of the GNU General Public License as published by
+ -  the Free Software Foundation, either version 3 of the License, or
+ -  (at your option) any later version.
+ -
+ -  This program is distributed in the hope that it will be useful,
+ -  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -  GNU General Public License for more details.
+ -
+ -  You should have received a copy of the GNU General Public License
+ -  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ -----------------------------------------------------------------------------*/
 
 package meshIneBits.gui.utilities;
 
@@ -59,7 +64,7 @@ public class AboutDialogWindow extends JDialog {
         // Visual options
         Image windowIcon = IconLoader.get("icon.png").getImage();
         this.setIconImage(windowIcon);
-        this.setSize(500, 300);
+        this.setSize(500, 375);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
@@ -73,6 +78,8 @@ public class AboutDialogWindow extends JDialog {
         bg.setFont(new Font(null, Font.BOLD | Font.ITALIC, 120));
         bg.setForeground(new Color(0, 0, 0, 8));
         bg.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+
 
         JLabel copyrightLabel0 = new JLabel("Copyright (C) 2016-2022 DANIEL Laurent.");
         copyrightLabel0.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -94,6 +101,18 @@ public class AboutDialogWindow extends JDialog {
         copyrightLabel8.setAlignmentX(Component.CENTER_ALIGNMENT);
         JLabel copyrightLabel9 = new JLabel("Copyright (C) 2021 VANNIYASINGAM Mithulan.");
         copyrightLabel9.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel copyrightLabel10 = new JLabel("Copyright (C) 2015 Cédric Siourakhan");
+        copyrightLabel10.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel copyrightLabel11 = new JLabel("Copyright (C) 2016 Gabriel Magny");
+        copyrightLabel11.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel copyrightLabel12 = new JLabel("Copyright (C) 2021  Quang Long");
+        copyrightLabel12.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel copyrightLabel13 = new JLabel("Copyright (C) 2022 Mhamad Atlab");
+        copyrightLabel13.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel copyrightLabel14 = new JLabel("Copyright (C) 2022 Majed Hlaihel");
+        copyrightLabel14.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JLabel copyrightLabel15 = new JLabel("Copyright (C) 2026 Aymeric Sirejol");
+        copyrightLabel15.setAlignmentX(Component.CENTER_ALIGNMENT);
 
     JButton helpFileBtn = new JButton("Open help file (PDF format)");
     helpFileBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -101,6 +120,8 @@ public class AboutDialogWindow extends JDialog {
     jp.add(new JLabel(" "));
     jp.add(bg);
     jp.add(copyrightLabel0);
+    jp.add(copyrightLabel10);
+    jp.add(copyrightLabel11);
     jp.add(copyrightLabel1);
     jp.add(copyrightLabel2);
     jp.add(copyrightLabel3);
@@ -110,6 +131,10 @@ public class AboutDialogWindow extends JDialog {
     jp.add(copyrightLabel7);
     jp.add(copyrightLabel8);
     jp.add(copyrightLabel9);
+    jp.add(copyrightLabel12);
+    jp.add(copyrightLabel13);
+    jp.add(copyrightLabel14);
+    jp.add(copyrightLabel15);
     jp.add(new JLabel(" "));
     jp.add(helpFileBtn);
     this.getContentPane()
--- src/main/java/meshIneBits/gui/view3d/Processor/AssemblingShapeProvider.java
+++ src/main/java/meshIneBits/gui/view3d/Processor/AssemblingShapeProvider.java
@@ -1,3 +1,38 @@
+/*------------------------------------------------------------------------------
+ -  MeshIneBits is a Java software to disintegrate a 3d mesh (model in .stl)
+ -  into a network of standard parts (called "Bits").
+ -
+ -  Copyright (C) 2016-2026 DANIEL Laurent.
+ -  Copyright (C) 2015 Cédric Siourakhan
+ -  Copyright (C) 2016 Gabriel Magny
+ -  Copyright (C) 2016  CASSARD Thibault & GOUJU Nicolas.
+ -  Copyright (C) 2017-2018  TRAN Quoc Nhat Han.
+ -  Copyright (C) 2018 VALLON Benjamin.
+ -  Copyright (C) 2018 LORIMER Campbell.
+ -  Copyright (C) 2018 D'AUTUME Christian.
+ -  Copyright (C) 2019 DURINGER Nathan (Tests).
+ -  Copyright (C) 2020-2021 CLAIRIS Etienne & RUSSO André.
+ -  Copyright (C) 2020-2021 DO Quang Bao.
+ -  Copyright (C) 2021 VANNIYASINGAM Mithulan.
+ -  Copyright (C) 2021  Quang Long
+ -  Copyright (C) 2022 Mhamad Atlab
+ -  Copyright (C) 2022 Majed Hlaihel
+ -  Copyright (C) 2026 Aymeric Sirejol
+ -
+ -  This program is free software: you can redistribute it and/or modify
+ -  it under the terms of the GNU General Public License as published by
+ -  the Free Software Foundation, either version 3 of the License, or
+ -  (at your option) any later version.
+ -
+ -  This program is distributed in the hope that it will be useful,
+ -  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ -  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ -  GNU General Public License for more details.
+ -
+ -  You should have received a copy of the GNU General Public License
+ -  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ -----------------------------------------------------------------------------*/
+
 package meshIneBits.gui.view3d.Processor;
 
 public class AssemblingShapeProvider {
